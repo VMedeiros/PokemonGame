6429a80e16dc95a8e410a55e67c512be5f766507
Victor Medeiros <victor.leitao95@gmail.com>
2017-06-13 13:13:56 -0300

    Pokemon Game Example
    Pokemon Game with augmented reality

---
+++ Assets/pokemon1script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class pokemon1script : MonoBehaviour {
+
+	public Button attackButton1;
+	public GameObject hose;
+	// Use this for initialization
+	void Start () {
+		attackButton1.onClick.AddListener (attackButton1Down);
+		hose.transform.FindChild ("Smoke").gameObject.SetActive(false);
+	}
+	IEnumerator Wait(){
+		hose.transform.FindChild ("Smoke").gameObject.SetActive(true);
+		yield return new WaitForSeconds (1);
+		hose.transform.FindChild ("Smoke").gameObject.SetActive(false);
+	}
+
+	void attackButton1Down(){
+		StartCoroutine (Wait ());
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/pokemon2script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class pokemon2script : MonoBehaviour {
+
+	public Button attackButton2;
+	public GameObject hose;
+	// Use this for initialization
+	void Start () {
+		attackButton2.onClick.AddListener (attackButton2Down);
+		hose.transform.FindChild ("Glow").gameObject.SetActive(false);
+	}
+	IEnumerator Wait(){
+		hose.transform.FindChild ("Glow").gameObject.SetActive(true);
+		yield return new WaitForSeconds (1);
+		hose.transform.FindChild ("Glow").gameObject.SetActive(false);
+	}
+
+	void attackButton2Down(){
+		StartCoroutine (Wait ());
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
